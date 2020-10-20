427b5ecb455c45dbc33402b90d81e917f74d103f
MoisesPL <pantion.loza.moises@gmail.com>
2020-10-20 13:30:25 +0200

    Master angular ready

--- frontend/myCrossing/src/app/app.component.ts
+++ frontend/myCrossing/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'myCrossing';
+  titulo = '';
+  descripcion = '';
+  constructor(private http: HttpClient){}
+
+  ngOnInit (){
+    this.http.get("http://localhost:8080/hello", {responseType: 'text'}).subscribe((resp: any) => {this.titulo = resp})
+  }
+
 }
+
+
