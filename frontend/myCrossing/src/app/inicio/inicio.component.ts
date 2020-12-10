0b0969be5e0aed375cfc133280251237f6c8d9d1
Moipanloz <pantion.loza.moises@gmail.com>
2020-12-10 18:03:18 +0100

    Inicio Done + Footer Comp
    - "Parallax"
    - Base de botones
    - Image  hover switch para div1
    
    Base de footer

--- frontend/myCrossing/src/app/inicio/inicio.component.ts
+++ frontend/myCrossing/src/app/inicio/inicio.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent implements OnInit {
+export class InicioComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  imagen : number;
+
+
+
+
 
 }
