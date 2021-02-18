1670cc494a0f94788553411f5172ce742a8b0128
adrgrabar <adrgrabar@alum.us.es>
2021-02-18 20:45:22 +0100

    Mayoria de la vista creada

--- frontend/myCrossing/src/app/perfil/visitasemanal/visitasemanal.component.ts
+++ frontend/myCrossing/src/app/perfil/visitasemanal/visitasemanal.component.ts
@@ -8,8 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class VisitasemanalComponent implements OnInit {
 
   constructor() { }
-
+  lpa = "totakeke";
+  mpa = null;
+  xpa = null;
+  jpa = null;
+  vpa = null;
+  spa = null;
+  dpa = null;
+  lpr = null;
+  mpr = null;
+  xpr = null;
+  jpr = null;
+  vpr = null;
+  spr = null;
+  dpr = null;
   ngOnInit(): void {
   }
-
+  vacio(datos:string){
+    console.log(datos);
+  }
 }
