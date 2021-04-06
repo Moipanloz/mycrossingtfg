42fd9e5b25d1dd332debac842a99f8cd306ed41f
Moipanloz <pantion.loza.moises@gmail.com>
2021-04-06 21:25:50 +0200

    Informacion terminada
    Vista base de la información terminada

--- project/myCrossing/src/app/calc-nabos/calc-nabos.component.ts
+++ project/myCrossing/src/app/calc-nabos/calc-nabos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import analyze from 'ac-stalk-market-analyzer';
 
 @Component({
   selector: 'app-calc-nabos',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalcNabosComponent implements OnInit {
 
+  filtrosView : boolean[] = [false, false, false, false];
+
+  precios : Array<number> = [95,95,100,130,110,0,0,0,0,0,0,0,0,0];
+
   constructor() { }
 
   ngOnInit(): void {
+    const prices = [73, 72, 69, 80, 92, 112, 98, 95, 35, 32, 20, 17];
+    const pattern = analyze(this.precios);
+    console.log(pattern);
   }
 
+  pito(){
+    console.log('pito')
+  }
 }
