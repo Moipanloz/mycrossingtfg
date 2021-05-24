bb5d0bb2c04018dcf622abab055b18f536b00e89
Moipanloz <pantion.loza.moises@gmail.com>
2021-05-24 19:03:07 +0200

    Footer: creditos y Licencia TP

--- project/myCrossing/src/app/footer/footer.component.ts
+++ project/myCrossing/src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  showInfo : boolean = false;
+
+  @HostListener("window:scroll")
+  onScroll(){
+    this.showInfo = false;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  cierra(){
+    if(this.showInfo){
+      this.showInfo = false;
+    }
+  }
+
+  toggle(e: MouseEvent){
+    this.showInfo = !this.showInfo
+    e.stopPropagation();
+  }
 }
