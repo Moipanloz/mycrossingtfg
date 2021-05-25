4a3bd97c724a24254c3a0bd231978fdb4193033f
Moipanloz <pantion.loza.moises@gmail.com>
2021-05-25 18:08:13 +0200

    Deploy DB WIP

--- project/my-crossing-back/opendb.php
+++ project/my-crossing-back/opendb.php
@@ -3,24 +3,30 @@
 if(isset($_GET["testing"]) && $_GET["testing"] == 'true'){
   header('Access-Control-Allow-Origin: http://localhost:9876');
 }else{
-  header('Access-Control-Allow-Origin: http://localhost:4200');
+  //header('Access-Control-Allow-Origin: http://localhost:4200');
+  header('Access-Control-Allow-Origin: *');
 }
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 header('Access-Control-Allow-Methods: OPTIONS, PUT, DELETE, POST, GET');
 
-define("DB_SERVERNAME", "localhost");
-define("DB_USER", "mcadmin");
-define("DB_PASS", "thisismypass");
+//Si es para testing, se usan los datos en local
 if(isset($_GET["testing"]) && $_GET["testing"] == 'true'){
-  define("DB_NAME", "mycrossingtestdb");
+  define("DB_DATABASE", "mycrossingtestdb");
+  define("DB_USERNAME", 'mcadmin');
+  define("DB_PASSWORD", 'thisismypass');
+  define("DB_HOST", "localhost");
 }else{
-  define("DB_NAME", "mycrossingdb");
+  //Si no, se usan los de heroku
+  define("DB_USERNAME", getenv('DB_USERNAME'));
+  define("DB_PASSWORD", getenv('DB_PASSWORD'));
+  define("DB_DATABASE", getenv('DB_DATABASE'));
+  define("DB_HOST", getenv('DB_HOST'));
 }
 
 function connect(){
 
   // Create connection
-  $conn = new mysqli(DB_SERVERNAME, DB_USER, DB_PASS, DB_NAME);
+  $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
   // Check connection
   if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
