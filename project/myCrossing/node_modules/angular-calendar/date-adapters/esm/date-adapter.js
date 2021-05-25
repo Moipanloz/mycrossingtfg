402c2c01b450a4e7af27403638d5490db1e7f497
Adrian Gracia <adrgrabar@alum.us.es>
2021-05-25 23:02:17 +0200

    Eliminada carpeta heredada

--- project/myCrossing/node_modules/angular-calendar/date-adapters/esm/date-adapter.js
+++
@@ -1,7 +0,0 @@
-var DateAdapter = /** @class */ (function () {
-    function DateAdapter() {
-    }
-    return DateAdapter;
-}());
-export { DateAdapter };
-//# sourceMappingURL=date-adapter.js.map
