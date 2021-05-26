ce52cb974b088443667a75cae3c55c5d01de177b
Moipanloz <pantion.loza.moises@gmail.com>
2021-05-26 17:13:17 +0200

    Update server.js

--- project/my-crossing/server.js
+++ project/my-crossing/server.js
@@ -8,17 +8,24 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 //app.use(requireHTTPS);
 
-app.use(express.static('./dist/my-crossing'));
+app.use(express.static(__dirname + '/dist/my-crossing'));
+//app.use(express.static('./dist/my-crossing'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/my-crossing/'}),
-);
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/my-crossing/'}),
+// );
 
+app.get('/*', function(req,res){
+  const fullPath = path.join(__dirname + '/dist/my-crossing/index.html');
+  console.log(" Fetching from.." + fullPath);
+    res.sendFile(fullPath);
+})
 
 app.listen(process.env.PORT || 8080);
-console.log('Node server running on port 8080');
+console.log('Server started running..');
 
 
 // const http = require('http');
