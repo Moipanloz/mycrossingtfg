6eb3899e1121bcf50b2cfa33f78ffe9be42c1ba6
Moipanloz <pantion.loza.moises@gmail.com>
2021-07-18 13:22:24 +0200

    Descomentado cors server.js

--- project/my-crossing/server.js
+++ project/my-crossing/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-//var cors = require('cors');
+var cors = require('cors');
 const app = express();
 const path = require('path');
 
-// app.use(cors());
-// app.options('*', cors());
+app.use(cors());
+app.options('*', cors());
 
 app.use(express.static(__dirname + '/dist/my-crossing'));
 
