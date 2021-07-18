a23c638d63b26f58393e8c59b56183514ed59ca9
Moipanloz <pantion.loza.moises@gmail.com>
2021-07-18 15:28:10 +0200

    Update server.js

--- project/my-crossing/server.js
+++ project/my-crossing/server.js
@@ -4,11 +4,21 @@ const app = express();
 const path = require('path');
 
 app.use(cors());
+var whitelist = ['https://mycrossing.herokuapp.com'];
+var corsOptions = {
+  origin: function (origin, callback){
+    if(whitelist.indexOf(origin) != -1){
+      callback(null, true);
+    }else {
+      callback(new Error('No permitido por CORS'));
+    }
+  }
+}
 app.options('*', cors());
 
 app.use(express.static(__dirname + '/dist/my-crossing'));
 
-app.get('/*', function(req,res){
+app.get('/*', cors(corsOptions), function(req,res){
   const fullPath = path.join(__dirname + '/dist/my-crossing/index.html');
   console.log(" Fetching from.." + fullPath);
     res.sendFile(fullPath);
